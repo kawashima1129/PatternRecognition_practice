6ca45341f883a3c5785f4365d620818ff2e583ac
kawashima1129 <k.ktak1129@gmail.com>
2017-08-07 19:44:14 +0900

    [modified]make_blobsによるダミーデータの生成

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -7,6 +7,7 @@ Created on Mon Jul 31 13:20:04 2017
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot  as plt
+from sklearn.datasets import make_blobs
 
 plt.style.use('ggplot')
 
@@ -15,25 +16,22 @@ ax1 = plt.subplot2grid((2,2), (0,0), colspan=2)
 ax2 = plt.subplot2grid((2,2), (1,0))
 ax3 = plt.subplot2grid((2,2), (1,1))
 
-def data_make(N, draw_plot, is_confused, confuse_bin):
-    np.random.seed(1)
-    
-    feature = np.random.randn(N, 2)
-    df = pd.DataFrame(feature, columns = ['x', 'y'])
-    df['c'] = df.apply(lambda row : 1 if (5*row.x + 3*row.y - 1)>0 else 0,  axis=1)
-
-    if is_confused:
-        def get_model_confused(data):
-            c = 1 if (data.name % confuse_bin) == 0 else data.c
-            return c
-        df['c'] = df.apply(get_model_confused, axis = 1)
+def data_make(N, draw_plot):
+    X, Y = make_blobs(n_samples = 500, centers = 2, cluster_std = 3.0)
+    X = pd.DataFrame(X, columns=['x1', 'x2'])
+    m = X.mean()
+    s = X.std()
+    X = X.sub(m).div(s)#標準化
+    Y = pd.DataFrame(Y, columns=['y'])
+    df_data = pd.concat([X, Y], axis = 1)
+    print(df_data.head())
     
     if draw_plot:
-        ax2.scatter(x = df.x, y = df.y, c = df.c, alpha = 0.5)
-        ax2.set_xlim([df.x.min() -0.1, df.x.max() +0.1])
-        ax2.set_ylim([df.y.min() -0.1, df.y.max() +0.1])
+        ax2.scatter(x = df_data.x1, y = df_data.x2, c = df_data.y, alpha = 0.5)
+        ax2.set_xlim([df_data.x1.min() -0.1, df_data.x1.max() +0.1])
+        ax2.set_ylim([df_data.x2.min() -0.1, df_data.x2.max() +0.1])
     
-    return df
+    return df_data
 
 def sigmoid(z):
     return 1.0 / (1 + np.exp(-z))
@@ -44,7 +42,8 @@ def estimate_weight(df_data, initial_param, N):
     error = []
     w = initial_param
     eta = 0.1 #学習率の初期値
-    for i in range(10):        
+    L = 10#学習回数
+    for i in range(L):        
         data_list =[k for k in range(N)]
         np.random.shuffle(data_list)
         
@@ -59,7 +58,7 @@ def estimate_weight(df_data, initial_param, N):
         eta *= 0.9
         error.append(np.abs(predict - t))
     ax1.set_title('error')
-    ax1.plot(range(10), error)
+    ax1.plot(range(L), error)
     return w
 
 def draw_split_line(weight_vector):
@@ -72,22 +71,22 @@ def draw_split_line(weight_vector):
 
 def validate_prediction(df_data, weight_vector):
     a, b,c = weight_vector
-    df_data['prob'] = df_data.apply(lambda row: 1 if(a*row.x + b*row.y + c) > 0 else 0, axis = 1)
-    df_data['p'] = df_data.apply(lambda row: sigmoid(a*row.x + b*row.y + c), axis = 1)
+    df_data['prob'] = df_data.apply(lambda row: 1 if(a*row.x1 + b*row.x2 + c) > 0 else 0, axis = 1)
+    df_data['p'] = df_data.apply(lambda row: sigmoid(a*row.x1 + b*row.x2 + c), axis = 1)
     
     return df_data
 
 def draw_prob(df_data, weight_vector):
     df = validate_prediction(df_data, weight_vector)
-    z3_plot = ax3.scatter(df_data.x, df_data.y, c=df_data.p, cmap='Blues', alpha=0.6)
-    ax3.set_xlim([df_data.x.min() -0.1, df.x.max() +0.1])
-    ax3.set_ylim([df_data.y.min() -0.1, df.y.max() +0.1])
+    z3_plot = ax3.scatter(df_data.x1, df_data.x2, c=df_data.p, cmap='Blues', alpha=0.6)
+    ax3.set_xlim([df_data.x1.min() -0.1, df.x1.max() +0.1])
+    ax3.set_ylim([df_data.x2.min() -0.1, df.x2.max() +0.1])
     plt.colorbar(z3_plot, ax = ax3)
     ax3.set_title('plot colored by probability', size=16)
 
 if __name__ == "__main__":
     N = 500
-    df_data = data_make(N, draw_plot=True, is_confused = True, confuse_bin = 10)
+    df_data = data_make(N, draw_plot=True)
 
     weight_vector = np.random.rand(3)
     weight_vector = estimate_weight(df_data, weight_vector, N)
@@ -98,6 +97,6 @@ if __name__ == "__main__":
     
     draw_prob(df_data, weight_vector)
     
-    df_pred['TF'] = df_pred.apply(lambda r: 1 if(r.c == r.prob) else 0, axis =1)
+    df_pred['TF'] = df_pred.apply(lambda r: 1 if(r.y == r.prob) else 0, axis =1)
     print('Accuracy : {}'.format(df_pred['TF'].sum() / N))
      
